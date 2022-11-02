247ec84acf05c9594b19297ace0e284af6ae78b0
sonolibero.io <ciao@sonolibero.io>
2022-11-02 15:46:30 +0100

    project setup

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Die from "./Die";
+
+export default function App() {
+    return (
+        <main>
+            <div>App</div>
+            <Die />
+        </main>
+    )
+}
---
+++ Die.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Die() {
+    return (
+        <div>Die</div>
+    )
+}
