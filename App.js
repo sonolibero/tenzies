a1d4edd376484fe7bdd43fa744e8ff19255bf2b7
sonolibero.io <ciao@sonolibero.io>
2022-11-02 16:01:29 +0100

    roll dice

--- App.js
+++ App.js
@@ -2,10 +2,29 @@ import React from "react";
 import Die from "./Die";
 
 export default function App() {
+
+    const [dice, setDice] = React.useState(allNewDice())
+
+    function allNewDice() {
+        const newDice = []
+        for(let i=0; i<10; i++){
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+
+    function rollDice() {
+        setDice(allNewDice())
+    }
+
+    const diceElements = dice.map(die => <Die value={die} />)
+
     return (
         <main>
-            <div>App</div>
-            <Die />
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
         </main>
     )
 }
--- Die.js
+++ Die.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-export default function Die() {
+export default function Die(props) {
     return (
-        <div>Die</div>
+        <div className="die-face">
+            <h2 className="die-num">{props.value}</h2>
+        </div>
     )
 }
