4b317ed592f7d866c3d73b1edeeece7a442450ac
sonolibero.io <ciao@sonolibero.io>
2022-11-02 16:32:35 +0100

    props id

--- Die.js
+++ Die.js
@@ -5,7 +5,7 @@ export default function Die(props) {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
     return (
-        <div className="die-face" style={styles} onClick={props.holdDice}>
+        <div className="die-face" style={styles} onClick={props.holdDice(props.id)}>
             <h2 className="die-num">{props.value}</h2>
         </div>
     )
