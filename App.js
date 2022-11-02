dc2ec7dde160aabc9a9356d6b3f06260630596d1
sonolibero.io <ciao@sonolibero.io>
2022-11-02 17:35:41 +0100

    false tenzies

--- App.js
+++ App.js
@@ -42,6 +42,7 @@ export default function App() {
                     generateNewDie()
             }))
         } else {
+            setTenzies(false)
             setDice(allNewDice())
         }
     }
