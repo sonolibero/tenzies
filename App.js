e10f1d996c1768b4121ace18b3a10ec87850dac5
sonolibero.io <ciao@sonolibero.io>
2022-11-02 16:39:38 +0100

    flip isHeld

--- App.js
+++ App.js
@@ -23,7 +23,9 @@ export default function App() {
     }
 
     function holdDice(id) {
-        console.log(id)
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ? {...die, isHeld: !die.isHeld} : die
+        }))
     }
 
     const diceElements = dice.map(die => (
